96e278a27770a6acb9cf9fb10d0230eaa455d37a
Matthew Barry <komputerwiz.matt@gmail.com>
2016-11-17 01:43:20 -0600

    update website

--- web/js/main.js
+++ web/js/main.js
@@ -26,7 +26,39 @@
   $('.footer-toggle').on('click', function (evt) {
     evt.preventDefault();
     $('#footer-content').slideToggle(200);
-    $(this).find('i.fa').toggleClass('fa-angle-double-up').toggleClass('fa-angle-double-down')
+
+    var icon = $(this).find('i');
+    var iconName = icon.text();
+
+    if (iconName === 'arrow_drop_up') {
+      icon.text('arrow_drop_down');
+    } else {
+      icon.text('arrow_drop_up');
+    }
+  });
+
+  // collapsible navigation lists
+  $('.nav-collapsible li').each(function () {
+    var item = $(this);
+
+    if (item.children('ol,ul').length === 0) {
+      return;
+    }
+
+    var handle = $('<span>', { class: 'handle material-icons', text: item.children('ol,ul').is(':hidden') ? 'keyboard_arrow_right' : 'expand_more' }).on('click', function (evt) {
+      evt.stopImmediatePropagation();
+      item.toggleClass('expanded').children('ol,ul').slideToggle(200);
+      $(this).text(($(this).text() === 'expand_more') ? 'keyboard_arrow_right' : 'expand_more');
+    });
+
+    item.prepend(handle);
+  });
+
+  $(window).on('resize', function () {
+    $('.nav-collapsible li').each(function () {
+      $(this).find('.handle')
+        .text($(this).children('ol,ul').is(':hidden') ? 'keyboard_arrow_right' : 'expand_more');
+    });
   });
 
 })();
